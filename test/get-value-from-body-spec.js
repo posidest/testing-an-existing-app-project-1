d6639a169ba3eedfb32c0989e2567b9f50bdbffa
Alana <alana.lapoint@gmail.com>
2020-12-11 18:38:12 -0500

    added more tests

--- test/get-value-from-body-spec.js
+++ test/get-value-from-body-spec.js
@@ -10,12 +10,13 @@ describe("The getValueFromBody function", () => {
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
-
+    const test = getValueFromBody(body, key)
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value of ""
-    expect.fail('please write this test');
+    expect(test).to.equal("");
+
   });
 
   it('returns an empty string for a body without the key', () => {
@@ -26,12 +27,12 @@ describe("The getValueFromBody function", () => {
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
-
+    const test = getValueFromBody(body, key);
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value of ""
-    expect.fail('please write this test');
+    expect(test).to.equal("");
   });
 
   it('returns the value of the key in a simple body', () => {
@@ -41,27 +42,28 @@ describe("The getValueFromBody function", () => {
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
-
+const test = getValueFromBody(body, key);
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value of "Bess"
-    expect.fail('please write this test');
+    expect(test).to.equal("Bess")
   });
 
   it('returns the value of the key in a complex body', () => {
     const body = "name=Bess&age=29&job=Boss";
+    const key = "age";
     // Select one of the keys in the body
 
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
-
+    const test = getValueFromBody(body, key)
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value for the key that you selected
-    expect.fail('please write this test');
+    expect(test).to.equal("29");
   });
 
   it('decodes the return value of URL encoding', () => {
